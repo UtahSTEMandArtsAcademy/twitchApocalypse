0f4d876f49daec7ad7ea20af75c35fc840c39a38
UtahSTEMandArtsAcademy <153949503+UtahSTEMandArtsAcademy@users.noreply.github.com>
2024-06-27 17:36:21 -0600

    Its rebase time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,5 +21,30 @@ public class GameManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void SetResoulution(int size)
+    {
+        switch (size)
+        {
+            case 0:
+                Screen.SetResolution(640,400,true);
+                break;
+            case 1:
+                Screen.SetResolution(1000,800,true);
+                break;
+            case 2:
+                Screen.SetResolution(1920,1080,true);
+                break;
+            case 3:
+                Screen.SetResolution(3840,2160,true);
+                break;
+            default:
+                Debug.Log("Not a Resolution");
+                break;
+        }//640x400
+        //1000x800
+        //1920x1080
+        //3840x2160
     }
 }
