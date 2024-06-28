d9afc64139c688b8dad9efee3c3d8d1672d8c157
CraftorK <kadenbronson14@gmail.com>
2024-06-27 18:52:39 -0600

    Added a Health Manager

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    public float healthPercentage
+    {
+        get { return health / maxHealth; }
+    }
+    public RectTransform healthBar;
+
+    public UnityEvent onDeath;
+    public UnityEvent onDamaged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (onDamaged != null)
+        {
+            onDamaged.Invoke();
+        }
+        if (health <= 0)
+        {
+            onDeath.Invoke();
+        }
+        UpdateHealthbar();
+    }
+
+    public void UpdateHealthbar()
+    {
+        healthBar.localScale = new Vector2(healthPercentage, 1);
+    }
+}
