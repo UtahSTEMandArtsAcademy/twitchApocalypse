294bd1a8ad9f2bdd61a3eda7e6f1dee8150e48cd
Admin <153949503+UtahSTEMandArtsAcademy@users.noreply.github.com>
2024-07-11 18:58:37 -0600

    worked on the pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PauseMenu : MonoBehaviour
+
 {
+    public InputActionAsset input;
+    public InputAction pause;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pause = input.FindActionMap("Wizard").FindAction("Pause");
+        pause.performed += e =>
+        {
+                       
+        };
+
     }
 
     // Update is called once per frame
